c2db8d2fe9d2b90c85c49964015aea4cdca0ad36
KarimZoPr0 <Hamsterhaunter@gmail.com>
2023-10-07 14:03:47 +0200

    ⚡ Adds SDL window initialization

--- main.c
+++ main.c
@@ -1,7 +1,40 @@
 #include <stdio.h>
+#include "stdbool.h"
+#include <SDL.h>
 
-int main( )
+SDL_Window *window = NULL;
+SDL_Renderer *renderer = NULL;
+
+bool initialize_window( void )
 {
-    printf("Hello, World!\n");
+    if( SDL_Init( SDL_INIT_EVERYTHING ) != 0 )
+    {
+        fprintf( stderr, "Error initializing SDL \n" );
+        return false;
+    }
+
+    window = SDL_CreateWindow( NULL, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600,
+                               SDL_WINDOW_ALWAYS_ON_TOP );
+    if( !window )
+    {
+        fprintf( stderr, "Error initializing SDL window. \n" );
+        return false;
+    }
+
+    renderer = SDL_CreateRenderer( window, -1, 0 );
+
+    if( !renderer )
+    {
+        fprintf( stderr, "Error initializing SDL renderer. \n" );
+        return false;
+    }
+
+    return true;
+}
+
+int main( int argc, char *argv[] )
+{
+    initialize_window( );
+
     return 0;
 }
