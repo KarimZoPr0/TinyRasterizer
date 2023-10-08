74594a44faafdeac8bedc3282e394d413b24e00e
KarimZoPr0 <Hamsterhaunter@gmail.com>
2023-10-08 14:15:25 +0200

    v0.0.7
    ✨Declares vector type

---
+++ include/vector.h
@@ -0,0 +1,22 @@
+//
+// Created by Karim on 2023-10-08.
+//
+
+#ifndef INC_3D_GRAPHICS_VECTOR_H
+#define INC_3D_GRAPHICS_VECTOR_H
+
+typedef struct
+{
+    float x;
+    float y;
+}vec2_t;
+
+typedef struct
+{
+    float x;
+    float y;
+    float z;
+}vec3_t;
+
+
+#endif //INC_3D_GRAPHICS_VECTOR_H
---
+++ src/vector.c
@@ -0,0 +1,7 @@
+//
+// Created by Karim on 2023-10-08.
+//
+
+#include "../include/vector.h"
+#include <math.h>
+
